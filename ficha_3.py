4650c816832b066bdf7fe696d1ee5bf9811f007c
Carlos <57544836+carmoran0@users.noreply.github.com>
2022-11-17 19:35:50 +0000

    Pending changes exported from your codespace

---
+++ ficha_3.py
@@ -0,0 +1,6 @@
+semana = {1:"lunes",2:"martes",3:"miercoles",4:"jueves",5:"viernes",6:"sabado",7:"domingo"}
+def programa():
+    nume = int(input("Número: "))
+    print("Es ", semana[nume])
+while True:
+    programa()
