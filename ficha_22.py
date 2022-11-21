f49e7d263d0089470ad31afa96251da8d64651c5
Carlos <57544836+carmoran0@users.noreply.github.com>
2022-11-21 15:58:53 +0000

    Pending changes exported from your codespace

---
+++ ficha_22.py
@@ -0,0 +1,26 @@
+# 22. Escriba una función que reciba una lista de 5 palabras y devuelva la que contenga más veces la letra a
+def main():
+    palabras = str(input("Escribe las palabras que quieras separadas por comas: "))
+    listapa  = palabras.split(",")
+    cuenta = []
+    for worf in listapa:
+        aas = 0
+        for letra in range(0, len(worf)):
+            letra = worf[letra]
+            if (letra.casefold() == 'a'):
+                print (letra, "es A")
+                aas = aas + 1
+        cuenta.append(aas)
+    print(cuenta)
+    valomax = max(cuenta)
+    print(valomax)
+    posfinal = cuenta.index(valomax)
+    print(posfinal)
+    print("LA PALABRA ES ", listapa[posfinal])
+
+        
+    
+   # print("La palabra con más A es: ", listapa[gana]) if cuenta[i] > cuenta[i+1]:
+
+while True:
+    main()
